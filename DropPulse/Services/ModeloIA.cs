68e1293bbb8c7521408fb06a7f2ee2914ccf1f57
susanalopez90 <sire0823@gimail.com>
2025-10-13 20:22:38 -0600

    "Susana: modelo IA creado y listo para AiClientService"

---
+++ DropPulse/Services/ModeloIA.cs
@@ -0,0 +1,14 @@
+﻿using DropPulse.Models;
+
+namespace DropPulse.Services
+{
+    public class ModeloIA
+    {
+        public bool DecidirSiRiega(SensorData datos)
+        {
+            // Si la humedad del suelo es menor a 30, se riega
+            return datos.SoilMoisture < 30;
+        }
+    }
+}
+
